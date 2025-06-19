001f8cda2a76cb6e21a24c9783acbab76c6269f3
Alejandro D. Bakker <AlejandroDimitri.Bakker@og1o.es>
2025-06-19 18:34:37 +0200

    DochubFrontend: add HTML5 & CSS files vesion 1

---
+++ PSTTG-Löschung_files/doc-hub-custom-javascript.js
@@ -0,0 +1,103 @@
+// Erstmeldung Datatable
+new DataTable('#doc-hub-erstmeldung-datatable', {
+  info: false,
+  ordering: true,
+  paging: false,
+  columnDefs: [
+    {targets: [0, 1, 2], className: "dt-header-left dt-body-left doc-hub-datable-header-left"},
+  ],
+  language: {
+    search: "Suchen:",
+  }
+});
+
+// Korrekturmeldung Datatable
+new DataTable('#doc-hub-korrekturmeldung-datatable', {
+  info: false,
+  ordering: true,
+  paging: false,
+  searching: false,
+  columnDefs: [
+    {targets: [0, 1], className: "dt-header-left dt-body-left doc-hub-datable-header-left"},
+    {
+      targets: 2,
+      orderable: true,
+      searchable: false,
+      className: 'doc-hub-select-checkbox',
+      render: function () {
+        return '<input type="checkbox">';
+      }
+    }
+  ],
+  select: {
+    style: 'os',
+  }
+});
+
+new DataTable('#doc-hub-korrekturmeldung-reference-child-datatable', {
+  info: false,
+  ordering: true,
+  paging: false,
+  searching: false,
+  columnDefs: [
+    {targets: [0, 1, 2], className: "dt-header-left dt-body-left doc-hub-datable-header-left"},
+    {
+      targets: 3,
+      orderable: true,
+      searchable: false,
+      className: 'doc-hub-select-checkbox',
+      render: function () {
+        return '<input type="checkbox">';
+      }
+    }
+  ],
+  select: {
+    style: 'os',
+  }
+});
+
+
+// Loschung Datatable
+new DataTable('#doc-hub-loschung-datatable', {
+  info: false,
+  ordering: true,
+  paging: false,
+  searching: false,
+  columnDefs: [
+    {targets: [0, 1], className: "dt-header-left dt-body-left doc-hub-datable-header-left"},
+    {
+      targets: 2,
+      orderable: true,
+      searchable: false,
+      className: 'doc-hub-select-checkbox',
+      render: function () {
+        return '<input type="checkbox">';
+      }
+    }
+  ],
+  select: {
+    style: 'os',
+  }
+});
+
+new DataTable('#doc-hub-loschung-reference-child-datatable', {
+  info: false,
+  ordering: true,
+  paging: false,
+  searching: false,
+  columnDefs: [
+    {targets: [0, 1, 2], className: "dt-header-left dt-body-left doc-hub-datable-header-left"},
+    {
+      targets: 3,
+      /*orderable: true,*/
+      searchable: false,
+      className: 'doc-hub-select-checkbox',
+      render: function () {
+        return '<input type="checkbox">';
+      }
+    }
+  ],
+  select: {
+    style: 'os',
+  }
+});
